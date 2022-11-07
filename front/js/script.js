8362291890d4d533020051371ab5dba0ed34a8b4
Rachid Marir <marirrachid@yahoo.fr>
2022-11-07 06:38:33 +0100

    add function appenChildran and makeAnchor

--- front/js/script.js
+++ front/js/script.js
@@ -3,16 +3,22 @@ fetch("http://localhost:3000/api/products")
     .then((data) => addProducts(data))
 
 function addProducts(data){
-    
-    const imageUrl= data[0].imageUrl
-                    
-         
-   const anchor = document.createElement ("a")                  //on cree la const anchor "a" qui donnera <a></a>
-    anchor.href = imageUrl                                      // on lui rajoute un lien 
-    anchor.text = "Canapé confortable"                          // on place un texte
+    const imageUrl = data[0].imageUrl       
 
-    const items = document.querySelector("#items")             //on cree la constante items
-    if (items != null){                                        //si items est non null
-        items.appendChild(anchor)                              //on lui donne un enfant qui est anchor (qui est la constente au dessus)
+    const anchor = makeAnchor(imageUrl) 
+    appendChildran(anchor)
+
+    }
+
+function makeAnchor(url){
+    const anchor = document.createElement ("a")                //on cree la const anchor "a" qui donnera <a></a>
+    anchor.href = url                                          // on lui rajoute un lien 
+    return anchor
+    }
+
+function appendChildran(anchor){
+        const items = document.querySelector("#items")             //on cree la constante items
+        if (items != null){                                        //si items est non null
+            items.appendChild(anchor)                              //on lui donne un enfant qui est anchor (qui est la constente au dessus)
         }
     }
