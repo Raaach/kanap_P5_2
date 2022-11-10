2decf865c97b2710720027b6f9892940331d79f0
Rachid Marir <marirrachid@yahoo.fr>
2022-11-10 14:18:07 +0100

    add product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,12 @@
-const queryString= window.location.search
-const urlParams = new URLSearchParams (queryString)
-const id = urlParams.get("id")
+const id = window.location.search
+const urlParams = new URLSearchParams(id)
+console.log(id)
 
- fetch("http://localhost:3000/api/products/_id")
+//const queryString= window.location.search
+//const urlParams = new URLSearchParams(queryString)
+//const id = urlParams.get("id")
+//console.log({id})
+
+ fetch("http://localhost:3000/api/products/{id}")
     .then((response)=> response.json())
     .then((res) => console.log(res))
