a7394edad41c20b3263d76f1cd84d981d0afcf87
Rachid Marir <marirrachid@yahoo.fr>
2022-11-11 23:11:42 +0100

    remove querrystring+ add fetch + function

--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,36 @@
-const id = window.location.search
-const urlParams = new URLSearchParams(id)
-console.log(id)
-
-//const queryString= window.location.search
+//const queryString = window.location.search     //"_id107fb5b75607497b96722bda5b504926"
+//console.log(queryString)
 //const urlParams = new URLSearchParams(queryString)
+//console.log(urlParams)
 //const id = urlParams.get("id")
-//console.log({id})
+//console.log(id)
 
- fetch("http://localhost:3000/api/products/{id}")
+
+fetch('http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926')              
     .then((response)=> response.json())
-    .then((res) => console.log(res))
+    .then((res) => handleData(res))
+
+function handleData(Kanap){
+    console.log({Kanap})
+    const {altTxt, colors, description, imageUrl,name, price, _id}= Kanap
+    makeImage(imageUrl, altTxt)
+    //makeColor(colors)
+    //makeDescription(description)
+    makeTitle(name)
+    //makePrice(_id)
+}
+
+function makeImage(imageUrl, altTxt){
+    const image = document.createElement('img')
+    image.src = imageUrl
+    image.alt = altTxt
+    const parent= document.querySelector(".item__img")
+    parent.appendChild(image)
+}
+function makeTitle(name){
+    const h1 = document.querySelector("#title")
+    h1.textContent = name
+}
+function makeColor(colors){
+    const color = document.createElement('div')
+}
