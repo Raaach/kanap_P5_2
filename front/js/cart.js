d4e36b989a1fe6e147fee6dbb21758e6eb89e946
Rachid Marir <marirrachid@yahoo.fr>
2023-04-29 20:58:02 +0200

    add com regex

--- front/js/cart.js
+++ front/js/cart.js
@@ -236,7 +236,7 @@ function isFormulaireInvalide(){
 
 function isEmailValide(){
   const email = document.querySelector("#email").value
-  const regex = /^[A-Za-z0-9+_.-]+@(.+)$/
+  const regex = /^[A-Za-z0-9+_.-]+@(.+)$/ //regex est une expresion décrit une syntaxe précise pour l'émail ici
   if (regex.test(email) === false) {
     alert("S'il vous plait entrez un email valide")
     return true
